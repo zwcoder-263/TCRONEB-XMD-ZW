7254d2401487d799798fe9c9d56d2dcb0d055b57
LiL CRONE <zwcoder263@gmail.com>
2025-02-10 15:04:09 -0800

    Update main-botInfo.js

--- tcronebcmds/main-botInfo.js
+++ tcronebcmds/main-botInfo.js
@@ -66,11 +66,11 @@ let handler = async (m, { conn, usedPrefix, command }) => {
 
 *≡ OWNER*
 ▢ Instagram :
-  • https://instagram.com/Lazack_28
+  • https://instagram.com/zwcoder
 ▢ GitHub :
-  • https://github.com/Lazack28 
+  • https://github.com/zwcoder-263
 ▢ YouTube : 
-  • https://youtube.com/@Lazack28
+  • https://youtube.com/@zwcoder
 ▢ credit: 
   • mr oreo
 
